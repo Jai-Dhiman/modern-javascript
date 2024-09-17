281abfd7b86bcbf86c52f6774828150de8b7ab08
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-16 17:50:51 -0700

    mathjs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-/* global moment */
-
 console.log("Hello");
+
+var moment = require("moment");
+import { sqrt } from "mathjs";
 var datetime = moment().startOf("day").fromNow();
 console.log(datetime);
+
+console.log(sqrt(4).toString());
