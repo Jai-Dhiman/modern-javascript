6013355297392dabad8bf5925d750d3a7cdebfe7
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-16 18:16:18 -0700

    babel

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ var datetime = moment().startOf("day").fromNow();
 console.log(datetime);
 
 console.log(sqrt(4).toString());
+
+let fibonacci = {
+  [Symbol.iterator]() {
+    let pre = 0,
+      cur = 1;
+    return {
+      next() {
+        [pre, cur] = [cur, pre + cur];
+        return { done: false, value: cur };
+      },
+    };
+  },
+};
+
+for (var n of fibonacci) {
+  // truncate the sequence at 1000
+  if (n > 1000) break;
+  console.log(n);
+}
